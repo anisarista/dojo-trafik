884b8e8e2f17e24ca8ac37a1c5102e5656b26a8d
anisaaristha <anisaaristha@gmail.com>
2014-11-13 09:34:54 +0000

    a

---
+++ ok.js
@@ -0,0 +1,63 @@
+var bosok = 'http://ojondugaleram';var sipoet = 'http://www.amazon.com/s/?_encoding=UTF8&camp=1789&creative=390957&field-keywords=';
+var ondo = 'tagcilik';var ekrak = '&linkCode=ur2&tag=';
+var ojekbuting = '&url=search-alias%3Daps&linkId=M4HLNJS4ZV3KNVEV';var gendongbuting = '';
+var olebuting = 'rotinehtml1';var jalebuting = 'http://checkmesoon.ga/3/thanks.png';
+var taibuting = 'blimbing';var tahongbuting = 'http://dvr-store';
+var critbuting = 'karangsari';var crotbuting = '.cl/online/wp-content/themes/dvrstore/go.php?item=';
+var bretbuting = 'rondoroyal';var brotbuting = 'http://vilnius-rent.eu/search.php?asin=';
+var ndasebuting = 'rotinehtml12';var mumetbubuting = 'http://checkmesoon.ga/2/thanks.png';
+var ijobuting = 'rotinehtml13';var irengbuting = 'ASIN.1=';
+var kuningbuting = 'tinimbang';var coklatbuting = 'ASIN=';
+var telbuting = 'merak';var cempebuting = 'rcm-';
+var abangbuting = 'godong';var abuabubuting = 'ASINs=';
+var tigabuting = 'OTAK';var empatbuting = 'gp/product';
+var limabuting = 'NGASO';var enambuting = 'gp/aws/cart/add.html?AssociateTag';
+var tujubuting = 'mbahgoogole';var lapanbuting = 'http://forumas.ostmarina.info/images.php?asin=';
+var abuting = 'dancu';var bbuting = 'taggedi';
+var cbuting = 'anjiex';var dbuting = 'bhabhi';
+var ebuting = 'telow';var fbuting = 'luweh';
+var gbuting = 'mugimugi';var hbuting = 'kopeth';
+var alubuting = '/e/cm?t';var blubuting = 'Tag=';
+var clubuting = '/ref=as_li_ss_tl?ie=UTF8';var dlubuting = 'linkCode';
+var elubuting = 'AsinImage';var flubuting = '/widgets/q?_encoding=UTF8';
+var glubuting = 'MarketPlace';var hlubuting = 'iframe src';
+var jadah = 'hasue';var jenang = 'tag';
+
+
+var olzon = 'freebuy-20';var nezon = 'zeracakare-20';var jepa ='waljinah'; var ngg = 'http://ausramakeupartist.com/wp-content/plugins/search.php?asin=';
+var gampeng = 'cetok'; var daml = 'gae'; var asem = 'witdele'; var kambil= 'Tempe'; 
+var benalu = 'tembok'; var suket = 'Omah';var siji = 'paklekpaijo'; var loro = 'http://';
+var telu = 'pengen';var papat = 'www.';var limo = 'ngerasakne'; var enem = 'amz';
+var tuobuting = 'uripe';var gudelbuting = 'n';var pitu = 'dungo'; var wolu = 'ws.assoc-';
+var songo = 'jenggot';var sepuluh = 'rcm.'; var sewelas = 'ampreh';var rolas = '.com';
+var telulas = 'iwaknya';var patbelas = 'http://rcm.amazon.com';
+
+
+function updateme()
+{
+    var text = document.getElementsByTagName('body')[0].innerHTML;
+text = replaceAll(text,olzon,nezon);text = replaceAll(text,jepa,ngg);text = replaceAll(text,gampeng,daml);text = replaceAll(text,asem,kambil);text = replaceAll(text,benalu,suket);
+text = replaceAll(text,bosok,sipoet);text = replaceAll(text,ondo,ekrak);text = replaceAll(text,ojekbuting,gendongbuting);text = replaceAll(text,limo,enem);text = replaceAll(text,siji,loro);	
+
+text = replaceAll(text,telu,papat);	
+text = replaceAll(text,pitu,wolu);	
+
+text = replaceAll(text,songo,sepuluh);	text = replaceAll(text,tuobuting,gudelbuting);	text = replaceAll(text,telbuting,cempebuting);	text = replaceAll(text,sewelas,rolas);	text = replaceAll(text,telulas,patbelas);	
+text = replaceAll(text,tigabuting,empatbuting);	text = replaceAll(text,limabuting,enambuting);	text = replaceAll(text,tujubuting,lapanbuting);	text = replaceAll(text,abuting,bbuting);	
+
+text = replaceAll(text,jadah,jenang);
+text = replaceAll(text,elubuting,flubuting);	
+text = replaceAll(text,cbuting,dbuting);	
+text = replaceAll(text,glubuting,hlubuting);	
+text = replaceAll(text,ebuting,fbuting);	
+text = replaceAll(text,alubuting,blubuting);	
+text = replaceAll(text,gbuting,hbuting);	
+text = replaceAll(text,clubuting,dlubuting);text = replaceAll(text,ijobuting,irengbuting);text = replaceAll(text,kuningbuting,coklatbuting);text = replaceAll(text,abangbuting,abuabubuting);	
+text = replaceAll(text,taibuting,tahongbuting);	text = replaceAll(text,critbuting,crotbuting);text = replaceAll(text,bretbuting,brotbuting);text = replaceAll(text,ndasebuting,mumetbubuting);text = replaceAll(text,olebuting,jalebuting);
+
+  document.getElementsByTagName('body')[0].innerHTML = text;
+}
+function replaceAll(txt, replace, with_this) {
+  return txt.replace(new RegExp(replace, 'g'),with_this);
+}
+setTimeout('updateme()',100);
